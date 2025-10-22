a0d2fcd4307e9fd730c285fb640fb379de5f7592
tae.creativelab <2.4taechasith.kangchuntod@gmail.com>
2025-10-22 16:12:47 +0700

    Day3

--- day-2.py
+++ day-2.py
@@ -1,1 +1,39 @@
-print("Hello") 
+#comment
+#function_name(input_1, input_2) 
+#argument is input of function
+# ctrl / make comment
+print('Hello')
+print('This is the second line')
+print(12, 6, 'Hello')
+print(12, 6, 'Hello', sep='-')
+
+'''
+we can add long comment
+'''
+
+'''
+Data Types
+- Number of student, name, money, yes or no
+
+Number
+- Integer: 3, 5, -4, 201 (int)
+- Floating Point: 3.25, -2.57, 5.00 (Decimal)
+
+Text
+- String: 'Hello', "Hello", "5.00"
+
+True, False (bool)
+- Boolean (truth value): True (T), False (F)
+
+Nonetype
+- None
+'''
+
+print(type(56))
+print(type(-2.0))
+print(type("-2.0"))
+
+'''
+Git is version control which work only in computer (เอาไว้เซฟงานเรื่อยๆ)
+GitHub คือ version internet
+'''
